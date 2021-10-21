83310b6e29515ba1a30f35a7c0d8d260b1ae63dd
Willem_O <w.ouwerkerk@kader.hcc.nl>
2021-10-21 21:57:51 +0200

    Update bbSPI MSP v100.f
    Updated SPI clock factorisation

--- Communication-Protocols/SPI/MSP430/noForth/bbSPI MSP v100.f
+++ Communication-Protocols/SPI/MSP430/noForth/bbSPI MSP v100.f
@@ -28,18 +28,20 @@ code READ-BIT   ( -- 0|1 )  tos sp -) mov  #0 tos mov  #2 28 & .b bia
 code WRITE-BIT  ( b -- )    tos day mov  sp )+ tos mov  80 # day bia  0<>?
                             if,  #4 29 & .b bis  else,  #4 29 & .b bic  then,
                             next  end-code
-code CLOCK      ( -- )      10 # 21 & .b bis 0 # day add  10 # 21 & .b bic
-                            next  end-code
+code CLOCK-HI               10 # 21 & .b bis  next  end-code
+code CLOCK-LOW      ( -- )  10 # 21 & .b bic  next  end-code
 code SPI}       ( -- )      #1 29 & .b bis  next  end-code
 code {SPI)      ( -- )      #1 29 & .b bic  next  end-code
 
 (* Write a bit to the SPI-bus, read abit from the SPI-bus
 
 : WRITE-BIT     ( b -- )    80 and if 4 29 *bis else 4 29 *bic then ; \ P2OUT
-: READ-BIT      ( -- 0|1 )  2 28 bit* 0<> 1 and ;           \ P2IN
-: CLOCK         ( -- )      10 21 *bis  10 21 *bic ;        \ P1OUT  SPI clock
-: SPI}          ( -- )      1 29 *bis ;                     \ P2OUT  SPI off, CS=high
-: {SPI)         ( -- )      1 29 *bic ;                     \ P2OUT  SPI on, CS=low
+: READ-BIT      ( -- 0|1 )  2 28 bit* 0<> 1 and ;   \ P2IN
+: CLOCK-HI      ( -- )      10 21 *bis ;            \ P1OUT  SPI clock
+: CLOCK-LOW     ( -- )      10 21 *bic ;            \ P1OUT  SPI clock
+: CLOCK         ( -- )      clock-hi  clock-low ;
+: SPI}          ( -- )      1 29 *bis ;             \ P2OUT  SPI off, CS=high
+: {SPI)         ( -- )      1 29 *bic ;             \ P2OUT  SPI on, CS=low
 
 *)
 
@@ -47,11 +49,12 @@ code {SPI)      ( -- )      #1 29 & .b bic  next  end-code
     8 for   dup write-bit  2*  clock  next  drop ;
 
 : SPI-IN            ( -- b )            \ Read b from SPI-bus
-    0  8 for  2*  read-bit or  next ;
+    0  8 for  2*  clock-hi  read-bit or  clock-low  next ;
 
 : SPI-I/O           ( b1 -- b2 )        \ Read and write at SPI-bus
     8 for
-        dup write-bit  2*   read-bit or  clock
+        dup write-bit  2*   clock-hi
+        read-bit or  clock-low
     next
     FF and ;
 
