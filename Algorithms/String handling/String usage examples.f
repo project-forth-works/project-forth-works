4c0b2e7b877c2c2312535a3ec7bac25680b16026
Willem_O <w.ouwerkerk@kader.hcc.nl>
2021-11-29 15:23:49 +0100

    Update String usage examples.f
    Updated to use string buffer

--- Algorithms/String handling/String usage examples.f
+++ Algorithms/String handling/String usage examples.f
@@ -1,14 +1,14 @@
 \ Usage examples for 'Primitive-string-word-set.f'
 
-create 'LIBRARY  31 allot
+20 $buffer LIBRARY$  21 allot
 
 : LIBPATH   ( -- a u )      s" c:\GenericForth\Lib" ;
-: .LIB      ( -- )          'library $@ $. ;
+: .LIB      ( -- )          library$ $@ $. ;
 
 : LIB-FILE" ( "name" -- )
-    libpath 'library $!           \ Set path to library files
-    [char] \ 'library $c+!        \ Add backslash
-    [char] " parse 'library $+! ; \ Add wanted file to path
+    libpath library$ $!           \ Set path to library files
+    [char] \ library$ $c+!        \ Add backslash
+    [char] " parse library$ $+! ; \ Add wanted file to path
     
 lib-file" Circular buffer V0.00.f"   .lib
 lib-file" PiliPlop.f"  .lib
