83f40db7ac6110080dd834ba6573863a36ea4be9
Willem_O <w.ouwerkerk@kader.hcc.nl>
2021-11-29 15:25:45 +0100

    Update Primitive-string-word-set.f
    Removed error from $BUFFER

--- Algorithms/String handling/Primitive-string-word-set.f
+++ Algorithms/String handling/Primitive-string-word-set.f
@@ -6,15 +6,15 @@
 \ Note: This code assumes that a character is byte wide!
 
 : $BUFFER       \ Reserve space for a string buffer
-    here  over 1+ allot     \ Reserve RAM buffer
+    here  swap 1+ allot     \ Reserve RAM buffer
     create  ( here) ,       ( +n "name" -- )
     does>  @ ;              ( -- s )
 
-: C+!       ( n a -- )      >r  r@ c@ +  r> c! ;    \ Incr. byte with n at a
-: $@        ( s -- c )      count ;                 \ Fetch string 'c' from 's'  
+: C+!       ( n a -- )      >r  r@ c@ +  r> c! ;        \ Incr. byte with n at a
+: $@        ( s -- c )      count ;                     \ Fetch string 'c' from 's'  
 : $+!       ( c s -- )      >r  tuck  r@ $@ +  swap cmove  r> c+! ; \ Extend string 's' with 'c'
-: $!        ( c s -- )      0 over c!  $+! ;        \ Store 'c' string into 's'
-: $.        ( c -- )        type ;                  \ Print string 'c'
-: $C+!      ( char s -- )   dup >r  $@ + c!  1 r> c+! ;             \ Add char to string 's'
+: $!        ( c s -- )      0 over c!  $+! ;            \ Store 'c' string into 's'
+: $.        ( c -- )        type ;                      \ Print string 'c'
+: $C+!      ( char s -- )   dup >r  $@ + c!  1 r> c+! ; \ Add char to string 's'
 
 \ End ;;;
