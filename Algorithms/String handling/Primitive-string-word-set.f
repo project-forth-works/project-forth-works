220103e3399de6a44a90effacca8ff39bde1336a
Willem_O <w.ouwerkerk@kader.hcc.nl>
2021-11-29 15:21:58 +0100

    Update Primitive-string-word-set.f
    Added string buffer defining word

--- Algorithms/String handling/Primitive-string-word-set.f
+++ Algorithms/String handling/Primitive-string-word-set.f
@@ -5,6 +5,11 @@
 \ s (c-addr) is the string, c (c-addr u) is constant string
 \ Note: This code assumes that a character is byte wide!
 
+: $BUFFER       \ Reserve space for a string buffer
+    here  over 1+ allot     \ Reserve RAM buffer
+    create  ( here) ,       ( +n "name" -- )
+    does>  @ ;              ( -- s )
+
 : C+!       ( n a -- )      >r  r@ c@ +  r> c! ;    \ Incr. byte with n at a
 : $@        ( s -- c )      count ;                 \ Fetch string 'c' from 's'  
 : $+!       ( c s -- )      >r  tuck  r@ $@ +  swap cmove  r> c+! ; \ Extend string 's' with 'c'
