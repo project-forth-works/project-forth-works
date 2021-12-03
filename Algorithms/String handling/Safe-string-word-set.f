5ebc28976877fe4a6ae41686d3a89470517fb184
Willem_O <w.ouwerkerk@kader.hcc.nl>
2021-12-03 17:47:41 +0100

    Update Safe-string-word-set.f

--- Algorithms/String handling/Safe-string-word-set.f
+++ Algorithms/String handling/Safe-string-word-set.f
@@ -1,7 +1,7 @@
 \  A safe minimal string package, original Albert van der Horst, this version W.O. 2021
 \
 \ Note that to keep security working, use the string buffer and operations together!
-\ This code also has a depency it uses S" interactivly!
+\ This code also has a depency, it uses S" interactivly!
 \
 \ The idea of strings is that a character string (s)
 \ is in fact a counted string (c) that has been stored.
